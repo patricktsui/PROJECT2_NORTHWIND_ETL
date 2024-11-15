28b386e3fd9398cbd1e520ae50fdcd0e3027273f
Patrick Tsui <patrick.tcm@gmail.com>
2024-11-15 15:17:58 +1100

    Initial commit

---
+++ northwind_dbt/models/marts/dim_customers.sql
@@ -0,0 +1,18 @@
+ITH source AS (
+    SELECT * FROM {{ source('northwind', 'customers') }}
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_key,
+    customer_id,
+    company_name,
+    contact_name,
+    contact_title,
+    address,
+    city,
+    region,
+    postal_code,
+    country,
+    phone,
+    fax
+FROM source
