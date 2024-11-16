4bb734753203e06b48bff331888c3c0808b2329c
Patrick Tsui <patrick.tcm@gmail.com>
2024-11-16 20:19:08 +1100

    Adding ecsTaskDefination

---
+++ data_transformation/northwind_dbt/upload_artifacts.py
@@ -0,0 +1,20 @@
+# upload_artifacts.py
+import os
+import boto3
+from datetime import datetime
+
+s3 = boto3.client('s3')
+
+def upload_directory(path, bucket, destination):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            local_path = os.path.join(root, file)
+            relative_path = os.path.relpath(local_path, path)
+            s3_path = os.path.join(destination, relative_path)
+            s3.upload_file(local_path, bucket, s3_path)
+
+bucket_name = 'ptsui-northwind-dbt-artifacts'
+artifact_path = '/usr/app/dbt/target'
+destination = f'dbt-artifacts/{datetime.now().strftime("%Y-%m-%d")}'
+
+upload_directory(artifact_path, bucket_name, destination)
